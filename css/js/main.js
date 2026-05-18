505eeaa44f9d41bf0b124c90cd3cb90f521364d7
Abhisek Ghosal <abhisek@admins-MacBook-Air-3.local>
2026-05-18 00:12:55 -0500

    updated

--- css/js/main.js
+++ css/js/main.js
@@ -3,11 +3,11 @@ window.addEventListener('scroll', () => {
   const navbar = document.querySelector('.navbar');
 
   if(window.scrollY > 50) {
-    navbar.style.background = 'rgba(255,255,255,0.98)';
+    navbar.style.background = 'rgba(78, 42, 132, 0.98)';
     navbar.style.boxShadow = '0 4px 12px rgba(0,0,0,0.08)';
   }
   else {
-    navbar.style.background = 'rgba(255,255,255,0.95)';
+    navbar.style.background = 'rgba(78, 42, 132, 0.96)';
     navbar.style.boxShadow = 'none';
   }
 
